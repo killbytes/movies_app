d218e571bc3e9afc5910104df1b993e82ef937e8
rrain <rrainpath@gmail.com>
2024-03-24 09:01:17 +0800

    added support for live typescript checking & added ability to use svg as react component

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    checker({
+      // use TypeScript check
+      typescript: true,
+    })
+  ],
 });
