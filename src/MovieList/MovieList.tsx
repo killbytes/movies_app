daa5b33b6ba66c051fcf812237aee765f992e35b
killcodings <kontaktid@ya.ru>
2024-03-30 02:10:38 +0300

    Add Card

--- src/MovieList/MovieList.tsx
+++ src/MovieList/MovieList.tsx
@@ -4,6 +4,19 @@ import { getMovies } from 'src/GetData';
 
 import css from './MoviesList.module.scss';
 
+
+import { Button, Card, Flex, Typography } from 'antd';
+
+const cardStyle: React.CSSProperties = {
+  width: 620,
+};
+
+const imgStyle: React.CSSProperties = {
+  display: 'block',
+  width: 273,
+};
+
+
 export type TMovie = {
   id: number;
 };
@@ -54,12 +67,42 @@ class MovieList extends React.Component<TMovie, TMoviePage> {
     const { movies, loading } = this.state;
     return (
       <div className={css.page}>
+
+
         <div>movies list</div>
         <button type="button" onClick={() => this.setState({ needToLoadMovies: true })}>
           reload list
         </button>
         <div>loading: {`${loading}`}</div>
-        {movies?.results.map((movie) => <div key={movie.id}>{JSON.stringify(movie)}</div>)}
+        <div className="cards">
+          {movies?.results.map((movie) => {
+            return (
+              // <div key={movie.id}>{JSON.stringify(movie)}</div>
+              <Card key={movie.id} hoverable style={cardStyle} styles={{ body: { padding: 0, overflow: 'hidden' } }}>
+                <Flex justify="space-between">
+                  <img
+                    alt="avatar"
+                    src={"https://image.tmdb.org/t/p/original" + (JSON.stringify(movie.poster_path)).replace(/^['"](.*)['"]$/, '$1')}
+                    style={imgStyle}
+                  />
+
+                  <Flex vertical align="flex-end" justify="space-between" style={{ padding: 32 }}>
+                    <Typography.Title level={3}>
+                      {JSON.stringify(movie.title)}
+                    </Typography.Title>
+
+                    <Typography.Title level={4}>
+                      {JSON.stringify(movie.overview)}
+                    </Typography.Title>
+                    <Button type="primary" href="https://ant.design" target="_blank">
+                      Get Started
+                    </Button>
+                  </Flex>
+                </Flex>
+              </Card>
+            )
+          } )}
+        </div>
       </div>
     );
   }
